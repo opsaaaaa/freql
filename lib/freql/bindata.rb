a77fa723a96efdd3901ac3b623c4fcf0e0c13e26
opsaaaaa <sean@ferney.org>
2023-06-06 14:26:30 -0600

    read lang file bindata

--- lib/freql/bindata.rb
+++ lib/freql/bindata.rb
@@ -1,3 +1,5 @@
+require 'zlib'
+require 'msgpack'
 
 module Freql
   module BinData
@@ -9,6 +11,9 @@ module Freql
 
     # its use in the [wordfq](https://github.com/rspeer/wordfq) program
     # to compress word frequency data.
+
+
+    LANG_FILE_PATH = "lib/freql/data/%s_%s.msgpack.gz"
 
     class << self
 
@@ -33,6 +38,18 @@ module Freql
         return hash_data
       end
 
+      def read_lang lang = :en, size: :small, &block
+        Zlib::GzipReader.open(LANG_FILE_PATH % [size,lang]) do |gz|
+          # The first item in the language data contains version and format information
+          # Im choosing to ignore that information for now.
+          # The rest of the data is word frequency bindata
+          block.call MessagePack.unpack(gz.read)[1..]
+        end
+      end
+
+      def read_and_unpack_lang lang = :en, size: :small, &block
+        read_lang(lang, size: size) {|data| block.call( unpack(data) ) }
+      end
     end
   end
 end
