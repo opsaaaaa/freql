b72f099cc0ccf5112e0786d201a3c3d113a9e5ba
opsaaaaa <sean@ferney.org>
2023-06-21 19:13:22 -0600

    compile graphemes executable

---
+++ bin/compile_en_grapheme_lists.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "bundler/setup"
+require 'freql/bindata'
+require 'freql/counter'
+require 'pry'
+
+
+[1,2,3,4,5].each do |token_size|
+  counter = Freql::Counter.new
+  bin = Freql::BinData.read_lang(:en, size: :small)
+  bin.remove_non_words!
+
+  bin.each_item do |item, cb|
+    counter.add_inflated_pairs(item, token_size)
+  end
+
+  graphemes = counter.compute_bindata
+
+  graphemes.write_lang(:en, size: "graphemes_#{token_size}")
+  puts "worte #{token_size} graphemes pack"
+
+  File.write("tmp/graphemes_#{token_size}.txt", graphemes.map {|i| i.join ' ' }.join("\n"))
+end
+
